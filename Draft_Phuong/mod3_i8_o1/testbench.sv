030cf628224445c14a091f567e1e5cc4102e99c3
phuong27102000 <75479195+phuong27102000@users.noreply.github.com>
2021-10-01 05:44:10 -0700

    Update testbench.sv

--- Draft_Phuong/mod3_i8_o1/testbench.sv
+++ Draft_Phuong/mod3_i8_o1/testbench.sv
@@ -1,21 +1,21 @@
 `include "mod3_i8_o1.sv"
 module testbench;
-  reg [7:0] a;
-  wire[7:0] out;
+  reg rst, clk;
+  reg[7:0] in;
+  wire[1:0] out;
   integer i;
   
-  inc_i8_o8 INC (a,out);
+  mod3_i8_o1 MOD3 (rst, clk, in, out);
   
-  initial begin 
-    i = 0;
-    forever begin
-      a = i; #1;
-      i = i + 1;
-    end
-  end
-  
+  always #1 clk = ~clk;
+    
   initial begin
-    #300;
+    clk = 0;
+    for (i=0; i < 256; i=i+1) begin
+      rst = 1;
+      in = i; #2;
+      rst = 0;#10;
+    end
     $finish;
   end
   
