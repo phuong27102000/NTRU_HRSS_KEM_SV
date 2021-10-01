fcd567f6abdd0bed43ef423bf896ed96a9dc70e9
phuong27102000 <75479195+phuong27102000@users.noreply.github.com>
2021-10-01 05:22:51 -0700

    Create testbench.sv

---
+++ Draft_Phuong/mod3_i8_o1/testbench.sv
@@ -0,0 +1,27 @@
+// Code your testbench here
+// or browse Examples
+module testbench;
+  reg [7:0] a;
+  wire[7:0] out;
+  integer i;
+  
+  inc_i8_o8 INC (a,out);
+  
+  initial begin 
+    i = 0;
+    forever begin
+      a = i; #1;
+      i = i + 1;
+    end
+  end
+  
+  initial begin
+    #300;
+    $finish;
+  end
+  
+  initial begin 
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+  end
+endmodule
