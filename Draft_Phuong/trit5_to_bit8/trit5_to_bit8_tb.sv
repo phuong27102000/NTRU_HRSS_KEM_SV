a1ae2d0cdc0a3fba2cd8122cc2886901e8e1b1e3
phuong27102000 <75479195+phuong27102000@users.noreply.github.com>
2021-10-03 19:07:28 -0700

    Update trit5_to_bit8_tb.sv

--- Draft_Phuong/trit5_to_bit8/trit5_to_bit8_tb.sv
+++ Draft_Phuong/trit5_to_bit8/trit5_to_bit8_tb.sv
@@ -3,27 +3,19 @@
 module trit5_to_bit8_tb(rst, clk, a, out, x, z, done);
     input rst, clk;
     input [9:0] a;
-    output done;
+    output reg done;
     output [7:0] out, x, z;
     reg[1:0] count;
     wire[1:0] count_new;
-    reg done;
     
-    always @(posedge clk) begin
-        if ( rst&(~done) )
-            count <= 0;
+    always @(posedge clk)
+        if (rst)
+            count <= 3;
         else
             count <= count_new;
-    end
     
-    
-    always @(negedge clk) begin
-        if (rst)
-            done <= 0;
-        else if (count == 3)
-            done <= 1;
-    end
-        
+    always @(negedge clk)
+        done <= (~rst) & (&count);
     
     inc_i2_o2 INC ( .a( count ), .out( count_new ) );
     trit5_to_bit8 TRIT2BIT (rst, clk, a, out, count, x, z);
