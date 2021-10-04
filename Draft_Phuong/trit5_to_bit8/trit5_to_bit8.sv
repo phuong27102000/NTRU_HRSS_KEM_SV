23b88bba8b0c9e2a90f297c660a3df74f8889aaf
phuong27102000 <75479195+phuong27102000@users.noreply.github.com>
2021-10-03 19:05:45 -0700

    Update trit5_to_bit8.sv

--- Draft_Phuong/trit5_to_bit8/trit5_to_bit8.sv
+++ Draft_Phuong/trit5_to_bit8/trit5_to_bit8.sv
@@ -4,17 +4,19 @@
 module trit5_to_bit8(rst, clk, a, out, count, x, z);
 //transform a 5-trit to an 8-bit
 //positive rst
-//negtive clk
+//positive clk
 //use external count, positive clk
-//after 4 periods, return the output
-//rst must be positive at least only half a period (recommend a period) to reset the instantiation
+//rst must be turned off after in taking datas at least 1 period of clk
+//after 1 reset period + 4 periods, return the output
+    
     input [9:0] a;
     input [1:0] count;
     input rst,clk;
     output [7:0] out, x, z;
     reg [6:0] _3_pow_1,  _3_pow_2, _3_pow_3, _3_pow_4;
-    wire[7:0] x1, x2, x3, x4, x, z;
-    reg[7:0] y0, y1, y2, y3, out;
+    wire[7:0] x0, x1, x2, x3, x4, x, z;
+    reg [9:0] a_reg;
+    reg[7:0] out;
     
     always @(posedge clk)
         if (rst) begin
@@ -22,26 +24,23 @@ module trit5_to_bit8(rst, clk, a, out, count, x, z);
             _3_pow_2 <= 9; 
             _3_pow_3 <= 27; 
             _3_pow_4 <= 81;
+            a_reg <= a;
         end
     always @(negedge clk)
-        if (rst) begin
-            y0 <= {a[1] & a[0], a[1] ^ a[0] };
-            y1 <= x1;
-            y2 <= x2;
-            y3 <= x3;
+        if (rst)
             out <= x4;
-        end
         else
             out <= z;
             
     ppa_sk_i8_o8 ADD ( .x1( x ), .x2( out ), .out( z ) );
     
-    mux_4i8_o1 MUX ( .a3( y3 ), .a2( y2 ), .a1( y1 ), .a0( y0 ), .sel( count ), .out( x ) );
+    mux_4i8_o1 MUX ( .a3( x3 ), .a2( x2 ), .a1( x1 ), .a0( x0 ), .sel( count ), .out( x ) );
     
-    mul_trit REG1 ( .trit( a[9:8] ), .a( _3_pow_4 ), .out( x4 ) );
-    mul_trit REG2 ( .trit( a[7:6] ), .a( _3_pow_3 ), .out( x3 ) );
-    mul_trit REG3 ( .trit( a[5:4] ), .a( _3_pow_2 ), .out( x2 ) );
-    mul_trit REG4 ( .trit( a[3:2] ), .a( _3_pow_1 ), .out( x1 ) );
+    assign x0 = {a_reg[1] & a_reg[0], a_reg[1] ^ a_reg[0] };
+    mul_trit REG1 ( .trit( a_reg[9:8] ), .a( _3_pow_4 ), .out( x4 ) );
+    mul_trit REG2 ( .trit( a_reg[7:6] ), .a( _3_pow_3 ), .out( x3 ) );
+    mul_trit REG3 ( .trit( a_reg[5:4] ), .a( _3_pow_2 ), .out( x2 ) );
+    mul_trit REG4 ( .trit( a_reg[3:2] ), .a( _3_pow_1 ), .out( x1 ) );
     
 endmodule
         
