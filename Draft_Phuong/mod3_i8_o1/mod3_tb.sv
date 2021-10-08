5a5e30f150dc669396f9377395d9bec37cf5fde4
phuong27102000 <75479195+phuong27102000@users.noreply.github.com>
2021-10-08 07:33:07 -0700

    Update mod3_tb.sv

--- Draft_Phuong/mod3_i8_o1/mod3_tb.sv
+++ Draft_Phuong/mod3_i8_o1/mod3_tb.sv
@@ -1,8 +1,8 @@
 `include "mod3_i8_o1.sv"
 `timescale 1ns/1ps
-module mod3_tb(input reg [7:0] in_sig,
-               input reg rst,
-               input reg clk, 
+module mod3_tb(input [7:0] in_sig,
+               input rst,
+               input clk, 
                output [1:0] out_sig);
     
     mod3_i8_o1 MOD3 (rst, clk, in_sig, out_sig);
