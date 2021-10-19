0e440a61d819fd1301d9ccd342c907ae60eb0908
phuong27102000 <75479195+phuong27102000@users.noreply.github.com>
2021-10-19 17:38:24 +0700

    Delete Draft_Phuong/Encaps/HashUnit directory

--- Draft_Phuong/Encaps/HashUnit/draft.py
+++
@@ -1,8 +0,0 @@
-import bit_handle
-map = {0: [0,0], 1: [0,1], -1: [1,1]}
-v = [-1, 1, 0, 1, -1]
-inp = []
-for i in v:
-    inp += map.get(i)
-inp = int.from_bytes( bit_handle.arr_to_str(inp), "big")
-print(inp)
