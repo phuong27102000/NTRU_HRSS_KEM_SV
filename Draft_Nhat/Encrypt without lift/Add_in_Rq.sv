df5cdad58df4820f7a2ab504d64dd0e901bb3d81
nhatdang1411 <74757824+nhatdang1411@users.noreply.github.com>
2021-10-29 19:56:02 +0700

    Add files via upload

---
+++ Draft_Nhat/Encrypt without lift/Add_in_Rq.sv
@@ -0,0 +1,12 @@
+module Add_in_Rq (
+	input logic [9113:1] v,
+	input logic [9113:1] m1,
+	output logic [9113:1] c
+	);
+	genvar i;
+	generate
+		for (i=1;i<=701;i=i+1) begin
+			carryselectadder entity_0 ( .in1(v[13*i:13*(i-1)+1]), .in2(m1[13*i:13*(i-1)+1]), .cin(1'b0) ,.out(c[13*i:13*(i-1)+1]));
+		end
+	endgenerate
+	endmodule
