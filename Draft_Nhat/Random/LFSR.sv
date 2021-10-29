6d788569e473fe8d00fc735e5d4fa33ce7a72c77
nhatdang1411 <74757824+nhatdang1411@users.noreply.github.com>
2021-10-29 22:28:33 +0700

    Update LFSR.sv

--- Draft_Nhat/Random/LFSR.sv
+++ Draft_Nhat/Random/LFSR.sv
@@ -1,19 +1,19 @@
 module LFSR (
 	input wire clk,
 	input wire rst,
-	output reg [16:1] coins
+	output reg [256:1] coins
 	);
 	integer i;
-	wire feedback_16= coins[16];
-	wire feedback_14= coins[14];
-	wire feedback_13= coins[13];	
-	wire feedback_11= coins[11];
+	wire feedback_256= coins[256];
+	wire feedback_32= coins[32];
+	wire feedback_22= coins[22];	
+	wire feedback_2= coins[2];
 	always @(posedge clk) begin
 		if (rst==1)
-			coins<=16'd65536;
+			coins<=256'd65536;
 		else begin
-			coins[1]<=feedback_16^feedback_14^feedback_13^feedback_11;
-			coins[16:2]<=coins[15:1];
+			coins[1]<=feedback_256^feedback_32^feedback_22^feedback_2;
+			coins[256:2]<=coins[256:1];
 		end
 	end
 endmodule
