533d304bcc00da99fa4b693e5ab141718ae4b219
nhatdang1411 <74757824+nhatdang1411@users.noreply.github.com>
2021-10-30 10:21:34 +0700

    Update LFSR.sv

--- Draft_Nhat/Random/LFSR.sv
+++ Draft_Nhat/Random/LFSR.sv
@@ -1,19 +1,38 @@
 module LFSR (
 	input wire clk,
 	input wire rst,
-	output reg [256:1] coins
+	output reg [8:1] coins,
+	output reg [8:1] coins_2
 	);
+	
 	integer i;
-	wire feedback_256= coins[256];
-	wire feedback_32= coins[32];
-	wire feedback_22= coins[22];	
-	wire feedback_2= coins[2];
+	
+	wire feedback_8_LFSR_1= coins[8];
+	wire feedback_6_LFSR_1= coins[6];
+	wire feedback_5_LFSR_1= coins[5];	
+	wire feedback_4_LFSR_1= coins[4];
+	
+	wire feedback_8_LFSR_2= coins_2[8];
+	wire feedback_6_LFSR_2= coins_2[6];
+	wire feedback_5_LFSR_2= coins_2[6];	
+	wire feedback_4_LFSR_2= coins_2[4];
+	
 	always @(posedge clk) begin
 		if (rst==1)
-			coins<=256'd65536;
+			coins<=8'd255;
 		else begin
-			coins[1]<=feedback_256^feedback_32^feedback_22^feedback_2;
-			coins[256:2]<=coins[256:1];
+			coins[1]<=feedback_8_LFSR_1^feedback_6_LFSR_1^feedback_5_LFSR_1^feedback_4_LFSR_1;
+			coins[8:2]<=coins[7:1];
 		end
 	end
+	
+	always @(posedge clk) begin
+		if (rst==1)
+			coins_2<=8'd1;
+		else begin
+			coins_2[1]<=feedback_8_LFSR_2^feedback_6_LFSR_2^feedback_5_LFSR_2^feedback_4_LFSR_2;
+			coins_2[8:2]<=coins_2[7:1];
+		end
+	end
+
 endmodule
