56ccccf36f4fe0e3838d4371fd6dd3cf174694a0
nhatdang1411 <74757824+nhatdang1411@users.noreply.github.com>
2021-10-30 10:49:12 +0700

    Update LFSR.sv

--- Draft_Nhat/Random/LFSR.sv
+++ Draft_Nhat/Random/LFSR.sv
@@ -8,21 +8,28 @@ module LFSR (
 	integer i;
 	
 	wire feedback_8_LFSR_1= coins[8];
+	wire feedback_7_LFSR_1= coins[7];
 	wire feedback_6_LFSR_1= coins[6];
 	wire feedback_5_LFSR_1= coins[5];	
 	wire feedback_4_LFSR_1= coins[4];
+	wire feedback_3_LFSR_1= coins[3];
+	wire feedback_2_LFSR_1= coins[2];
 	
 	wire feedback_8_LFSR_2= coins_2[8];
+	wire feedback_7_LFSR_2= coins_2[7];
 	wire feedback_6_LFSR_2= coins_2[6];
 	wire feedback_5_LFSR_2= coins_2[5];	
 	wire feedback_4_LFSR_2= coins_2[4];
+	wire feedback_3_LFSR_2= coins_2[3];
+	wire feedback_2_LFSR_2= coins_2[2];
 	
 	always @(posedge clk) begin
 		if (rst==1)
 			coins<=8'd255;
 		else begin
-			coins[1]<=feedback_8_LFSR_1^feedback_6_LFSR_1^feedback_5_LFSR_1^feedback_4_LFSR_1;
-			coins[8:2]<=coins[7:1];
+			coins[1] <= feedback_8_LFSR_1^feedback_6_LFSR_1^feedback_5_LFSR_1^feedback_4_LFSR_1;
+			coins[8:2] <= coins[7:1];
+			
 		end
 	end
 	
@@ -30,8 +37,8 @@ module LFSR (
 		if (rst==1)
 			coins_2<=8'd1;
 		else begin
-			coins_2[1]<=feedback_8_LFSR_2^feedback_6_LFSR_2^feedback_5_LFSR_2^feedback_4_LFSR_2;
-			coins_2[8:2]<=coins_2[7:1];
+			coins_2[1] <= feedback_8_LFSR_2^feedback_6_LFSR_2^feedback_5_LFSR_2^feedback_4_LFSR_2;
+			coins_2[8:2] <= coins_2[7:1];
 		end
 	end
 
